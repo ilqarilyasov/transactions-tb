a7582c1be41a0ba26b9a0c947f54dcfd9e8dc191
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-05-25 11:35:37 -0500

    Initial Commit

---
+++ Transactions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Transactions
+//
+//  Created by Ilgar Ilyasov on 5/25/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
